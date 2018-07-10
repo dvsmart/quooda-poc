14304a53e1a91d0e82b8688eb0437cd68094c126
Vijay Kumar <vjdeveloper29@gmail.com>
2018-07-10 18:05:51 +0100

    application name change and properties list working

--- src/app/features/asset/components/properties/properties.component.ts
+++ src/app/features/asset/components/properties/properties.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TableConfig } from '@app/shared/models/TableConfig';
 
 @Component({
   selector: 'app-properties',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./properties.component.scss']
 })
 export class PropertiesComponent implements OnInit {
-
+  columnsConfig: TableConfig;
   constructor() { }
 
   ngOnInit() {
+    this.columnsConfig = new TableConfig(5);
+    this.columnsConfig.url = 'AssetProperties';
+    const columns =
+      [
+        {
+          primaryKey: 'dataId',
+          header: 'ID'
+        },
+        {
+          primaryKey: 'addressLine1',
+          header: 'Address Line 1'
+        },
+        {
+          primaryKey: 'postCode',
+          header: 'Post Code'
+        },
+        {
+          primaryKey: 'city',
+          header: 'City'
+        },
+        {
+          primaryKey: 'portfolioName',
+          header: 'Portfolio'
+        },
+        {
+          primaryKey: 'subPortfolioName',
+          header: 'SubPortfolio'
+        },
+        {
+          primaryKey: 'addedDate',
+          header: 'Added On',
+          format: 'date'
+        },
+        {
+          primaryKey:'assetType',
+          header:'Asset Type'
+        }
+      ];
+    this.columnsConfig.columns = columns;
   }
-
 }
