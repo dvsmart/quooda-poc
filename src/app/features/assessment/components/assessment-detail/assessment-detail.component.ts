0827d41a486e9a7f5b05abb11a3ddb669d23845f
dvsmart <36995044+dvsmart@users.noreply.github.com>
2018-07-28 00:26:54 +0100

    added select fields

--- src/app/features/assessment/components/assessment-detail/assessment-detail.component.ts
+++ src/app/features/assessment/components/assessment-detail/assessment-detail.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AssessmentService } from '@app/features/assessment/service/assessment.service';
 import { ToasterService } from '@app/shared/services/toaster.service';
 import { MessageService } from '@app/shared/services/message.service';
+import { ReferenceModel } from '@app/features/assessment/model/ReferenceModel';
 
 @Component({
   selector: 'app-assessment-detail',
@@ -13,10 +14,17 @@ export class AssessmentDetailComponent implements OnInit {
   formGroup: FormGroup;
   @Input() data: any;
   title: string;
+  scopes: ReferenceModel[];
+
   constructor(private assessmentservice: AssessmentService, private toaster: ToasterService,private messageservice: MessageService) { }
 
   ngOnInit() {
     this.createFormGroup();
+    this.getScopes();
+  }
+
+  getScopes(){
+    this.assessmentservice.getscopes().subscribe(x=> this.scopes = x);
   }
 
   createFormGroup() {
@@ -25,9 +33,11 @@ export class AssessmentDetailComponent implements OnInit {
       reference: new FormControl('', Validators.required),
       type: new FormControl('', Validators.required),
       scope: new FormControl(''),
+      scopeId: new FormControl(''),
       status:new FormControl(''),
       assessmentDate:new FormControl(''),
-      id: new FormControl(0)
+      id: new FormControl(0),
+      publishedBy:new FormControl('')
     });
     if (this.data != null && this.data != undefined) {
       this.title = 'Edit Assessment - ' + this.data.dataId;
@@ -37,13 +47,22 @@ export class AssessmentDetailComponent implements OnInit {
         title: this.data.title,
         reference: this.data.reference,
         type: this.data.assessmentType,
-        scope: this.data.assessmentScope,
-        assessmentDate: this.data.assessmentDate
+        scope: this.data.scope,
+        assessmentDate: this.data.assessmentDate,
+        publishedBy: this.data.publishedBy,
+        scopeId:this.data.scopeId
       })
     } else {
       this.title = 'Create New Assessment';
     }
   }
+
+  compareFn: ((f1: any, f2: any) => boolean) | null = this.compareByValue;
+
+compareByValue(f1: any, f2: any) { 
+  debugger;
+  return f1 && f2 && f1.value === f2.value; 
+}
 
   ngOnChanges() {
     this.createFormGroup();
@@ -54,6 +73,7 @@ export class AssessmentDetailComponent implements OnInit {
   }
 
   save() {
+    debugger;
     if (this.formGroup.value.id == "") {
       this.assessmentservice.add(this.formGroup.value).subscribe(x => {
         if (x['saveSuccessful'] === true) {
--- src/app/features/assessment/service/assessment.service.ts
+++ src/app/features/assessment/service/assessment.service.ts
@@ -3,12 +3,14 @@ import { environment } from 'environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CreateAssessmentRequest } from '@app/features/assessment/model/CreateAssessmentRequest';
 import { Observable } from 'rxjs';
+import {ReferenceModel } from '../model/ReferenceModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssessmentService {
   api = environment.apiUrl + 'Assessment/';
+  referenceApi = environment.apiUrl + 'AssessmentReference/'
   constructor(private http: HttpClient) { }
 
   public add(propertyModel: CreateAssessmentRequest): Observable<boolean> {
@@ -21,5 +23,12 @@ export class AssessmentService {
 
   public getSingle(id: number): Observable<CreateAssessmentRequest> {
     return this.http.get<CreateAssessmentRequest>(this.api + id);
+  }
+
+  public getscopes(): Observable<ReferenceModel[]> {
+    return this.http.get<ReferenceModel[]>(this.referenceApi + 'Scopes');
+  }
+  public getTypes(): Observable<ReferenceModel[]> {
+    return this.http.get<ReferenceModel[]>(this.referenceApi + 'Types');
   }
 }
